797fceb2eaa1d8a2677352950593fc28fe88f434
Elise Ngo <elisetvy@gmail.com>
2024-03-20 15:35:29 -0700

    Create components folder & start NewNote component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import NewNote from "./components/NewNote";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="border border-solid border-red-400 mx-10 my-10">
       <Routes>
         <Route path="/" element={<p>Home</p>} />
-        <Route path="/new" element={<p>New</p>} />
+        <Route path="/new" element={<NewNote />} />
         <Route path="/:id">
           <Route index element={<p>Show</p>} />
           <Route path="edit" element={<p>Edit</p>} />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/NewNote.tsx
@@ -0,0 +1,9 @@
+function NewNote() {
+  return (
+    <>
+      <p>New Note</p>
+    </>
+  );
+}
+
+export default NewNote;
