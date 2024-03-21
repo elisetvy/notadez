67e41ec336ba85577ad5de6cac29cac0c9bd9a69
Elise Ngo <elisetvy@gmail.com>
2024-03-21 13:01:15 -0700

    Add refs

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -1,16 +1,25 @@
 import { Form, Stack, Row, Col, Button } from "react-bootstrap";
 import CreatableReactSelect from "react-select/creatable";
+import { Link } from "react-router-dom";
+import { useRef } from "react";
 
-function NoteForm() {
+function NoteForm({ onSubmit }) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
+
   return (
     <>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Stack gap={4}>
           <Row>
             <Col>
               <Form.Group controlId="title">
                 <Form.Label>Title</Form.Label>
-                <Form.Control required />
+                <Form.Control ref={titleRef} required />
               </Form.Group>
             </Col>
             <Col>
@@ -22,15 +31,17 @@ function NoteForm() {
           </Row>
           <Form.Group controlId="markdown">
             <Form.Label>Body</Form.Label>
-            <Form.Control required as="textarea" rows={15} />
+            <Form.Control ref={markdownRef} required as="textarea" rows={15} />
           </Form.Group>
           <Stack direction="horizontal" gap={2} className="justify-content-end">
             <Button type="submit" variant="primary">
               Save
             </Button>
-            <Button type="button" variant="outline-secondary">
-              Cancel
-            </Button>
+            <Link to="..">
+              <Button type="button" variant="outline-secondary">
+                Cancel
+              </Button>
+            </Link>
           </Stack>
         </Stack>
       </Form>
